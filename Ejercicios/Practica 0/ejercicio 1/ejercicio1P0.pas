d1f0ed6959bea511bb151867da758c81274982bd
imanol salinas <imanolsalinas1996@gmail.com>
2021-05-28 13:58:31 -0300

    primer ejercicio

---
+++ Ejercicios/Practica 0/ejercicio 1/ejercicio1P0.pas
@@ -0,0 +1,10 @@
+program ejercicio1P0;
+var
+  primerNumero,segundoNumero:integer;
+begin
+  writeln('Ingrese un numero');
+  readln(primerNumero);
+  writeln('Ingrese otro numero');
+  readln(segundoNumero);
+  writeln(segundoNumero, ' , ', primerNumero ,'.');
+end.
